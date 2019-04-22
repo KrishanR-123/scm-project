e3d0083c57c604ce687e0a2025fe34dba58cd468
SARTHAK JAIN <42979887+mrsarthak001@users.noreply.github.com>
2019-04-22 19:38:15 +0530

    UPDATE 1

--- school_management.c
+++ school_management.c
@@ -130,4 +130,140 @@ int main()
  
 }
 //krishan ends
+//sarthak starts
+void addrecord( )
+ 
+{
+ 
+                system("cls");
+ 
+                FILE *fp ;
+ 
+                char another = 'Y' ,time[10];
+ 
+                struct record e ;
+ 
+                char filename[15];
+ 
+                int choice;
+ 
+                printf("\n\n\t\t***************************\n");
+ 
+                printf("\t\t* WELCOME TO THE ADD MENU *");
+ 
+                printf("\n\t\t***************************\n\n");
+ 
+                printf("\n\n\tENTER DATE OF YOUR RECORD:[yyyy-mm-dd]:");
+ 
+                fflush(stdin);
+ 
+                gets(filename);
+ 
+                fp = fopen (filename, "ab+" ) ;
+ 
+                if ( fp == NULL )
+ 
+                {
+ 
+                fp=fopen(filename,"wb+");
+ 
+                if(fp==NULL)
+ 
+                {
+ 
+                    printf("\nSYSTEM ERROR...");
+ 
+                    printf("\nPRESS ANY KEY TO EXIT");
+ 
+                    getch();
+ 
+                    return ;
+ 
+                }
+ 
+                }
+ 
+                while ( another == 'Y'|| another=='y' )
+ 
+                {
+ 
+                    choice=0;
+ 
+                    fflush(stdin);
+ 
+                printf ( "\n\tENTER TIME:[hh:mm]:");
+ 
+                scanf("%s",time);
+ 
+                rewind(fp);
+ 
+                while(fread(&e,sizeof(e),1,fp)==1)
+ 
+                {
+ 
+                    if(strcmp(e.time,time)==0)
+ 
+                    {
+ 
+                        printf("\n\tTHE RECORD ALREADY EXISTS.\n");
+ 
+                        choice=1;
+ 
+                    }
+ 
+                }
+ 
+                    if(choice==0)
+ 
+                    {
+ 
+                        strcpy(e.time,time);
+ 
+                        printf("\tENTER NAME:");
+ 
+                        fflush(stdin);
+ 
+                        gets(e.name);
+ 
+                        fflush(stdin);
+ 
+                        printf("\tENTER PLACE:");
+ 
+                        gets(e.place);
+ 
+                        fflush(stdin);
+ 
+                        printf("\tENTER DURATION:");
+ 
+                        gets(e.duration);
+ 
+                        fflush(stdin);
+ 
+                        printf("\tNOTE:");
+ 
+                        gets(e.note);
+ 
+                        fwrite ( &e, sizeof ( e ), 1, fp ) ;
+ 
+                        printf("\nYOUR RECORD IS ADDED...\n");
+ 
+                    }
+ 
+                        printf ( "\n\tADD ANOTHER RECORD...(Y/N) " ) ;
+ 
+                        fflush ( stdin ) ;
+ 
+                        another = getchar( ) ;
+ 
+                }
+ 
+                fclose ( fp ) ;
+ 
+                printf("\n\n\tPRESS ANY KEY TO EXIT...");
+ 
+                getch();
+ 
+    }
+//sarthak ends
+
 
