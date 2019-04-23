e4b8199715e9441395c918b8066962248caf07a8
Kshitiz Saini <40308662+kshitizsaini113@users.noreply.github.com>
2019-04-23 21:54:22 +0530

    Viewrecord function created

--- school_management.c
+++ school_management.c
@@ -131,3 +131,154 @@ int main()
 }
 //krishan ends
 
+void viewrecord( )
+ 
+{
+ 
+        FILE *fpte ;
+ 
+        system("cls");
+ 
+        struct record customer ;
+ 
+        char time[6],choice,filename[14];
+ 
+        int ch;
+ 
+        printf("\n\n\t\t*******************************\n");
+ 
+        printf("\t\t* HERE IS THE VIEWING MENU *");
+ 
+        printf("\n\t\t*******************************\n\n");
+ 
+        choice=password();
+ 
+        if(choice!=0)
+ 
+            {
+ 
+                return ;
+ 
+            }
+ 
+        do
+ 
+            {
+ 
+                    printf("\n\tENTER THE DATE OF RECORD TO BE VIEWED:[yyyy-mm-dd]:");
+ 
+                    fflush(stdin);
+ 
+                    gets(filename);
+ 
+                    fpte = fopen ( filename, "rb" ) ;
+ 
+                    if ( fpte == NULL )
+ 
+                            {
+ 
+                                puts ( "\nTHE RECORD DOES NOT EXIST...\n" ) ;
+ 
+                                printf("PRESS ANY KEY TO EXIT...");
+ 
+                                getch();
+ 
+                                return ;
+ 
+                            }
+ 
+                            system("cls");
+ 
+                printf("\n\tHOW WOULD YOU LIKE TO VIEW:\n");
+ 
+                printf("\n\t1.WHOLE RECORD OF THE DAY.");
+ 
+                printf("\n\t2.RECORD OF FIX TIME.");
+ 
+                printf("\n\t\tENTER YOUR CHOICE:");
+ 
+                scanf("%d",&ch);
+ 
+                switch(ch)
+ 
+                {
+ 
+                    case 1:
+ 
+                            printf("\nTHE WHOLE RECORD FOR %s IS:",filename);
+ 
+                         while ( fread ( &customer, sizeof ( customer ), 1, fpte ) == 1 )
+ 
+                         {
+ 
+                            printf("\n");
+ 
+                            printf("\nTIME: %s",customer.time);
+ 
+                            printf("\nMEETING WITH: %s",customer.name);
+ 
+                            printf("\nMEETING AT: %s",customer.place);
+ 
+                            printf("\nDURATION: %s",customer.duration);
+ 
+                            printf("\nNOTE: %s",customer.note);
+ 
+                            printf("\n");
+ 
+                         }
+ 
+                         break;
+ 
+                    case 2:
+ 
+                            fflush(stdin);
+ 
+                            printf("\nENTER TIME:[hh:mm]:");
+ 
+                            gets(time);
+ 
+                            while ( fread ( &customer, sizeof ( customer ), 1, fpte ) == 1 )
+ 
+                            {
+ 
+                                if(strcmp(customer.time,time)==0)
+ 
+                                {
+ 
+                                    printf("\nYOUR RECORD IS:");
+ 
+                                    printf("\nTIME: %s",customer.time);
+ 
+                                    printf("\nMEETING WITH: %s",customer.name);
+ 
+                                    printf("\nMEETING AT: %s",customer.place);
+ 
+                                    printf("\nDUARATION: %s",customer.duration);
+ 
+                                    printf("\nNOTE: %s",customer.note);
+ 
+                                }
+ 
+                            }
+ 
+                            break;
+ 
+                    default: printf("\nYOU TYPED SOMETHING ELSE...\n");
+ 
+                             break;
+ 
+                }
+ 
+               printf("\n\nWOULD YOU LIKE TO CONTINUE VIEWING...(Y/N):");
+ 
+               fflush(stdin);
+ 
+            scanf("%c",&choice);
+ 
+        }while(choice=='Y'||choice=='y');
+ 
+            fclose ( fpte ) ;
+ 
+            return ;
+ 
+}
