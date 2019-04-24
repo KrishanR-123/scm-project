2e3425b50e37e08de5104902a5aa8905387e49c3
Kshitiz Saini <40308662+kshitizsaini113@users.noreply.github.com>
2019-04-24 22:43:47 +0530

    Update school_management.c

--- school_management.c
+++ school_management.c
@@ -434,3 +434,210 @@ void viewrecord( )
             return ;
  
 }
+
+
+void editpassword()
+ 
+{
+ 
+    system("cls");
+ 
+    printf("\n");
+ 
+    char pass[15]={0},confirm[15]={0},ch;
+ 
+    int choice,i,check;
+ 
+    FILE *fp;
+ 
+    fp=fopen("SE","rb");
+ 
+    if(fp==NULL)
+ 
+    {
+ 
+        fp=fopen("SE","wb");
+ 
+        if(fp==NULL)
+ 
+        {
+ 
+            printf("SYSTEM ERROR...");
+ 
+            getch();
+ 
+            return ;
+ 
+        }
+ 
+        fclose(fp);
+ 
+        printf("\nSYSTEM RESTORED...\nYOUR PASSWORD IS 'ENTER'\n PRESS ENTER TO CHANGE PASSWORD\n\n");
+ 
+        getch();
+ 
+    }
+ 
+        fclose(fp);
+ 
+        check=password();
+ 
+    if(check==1)
+ 
+    {
+ 
+        return ;
+ 
+    }
+ 
+    do
+ 
+    {
+ 
+        if(check==0)
+ 
+                    {
+ 
+                        i=0;
+ 
+                        choice=0;
+ 
+                        printf("\n\n\tENTER THE NEW PASSWORD:");
+ 
+                        fflush(stdin);
+ 
+                        pass[0]=getch();
+ 
+                        while(pass[i]!='\r')
+ 
+                        {
+ 
+                            if(pass[i]=='\b')
+ 
+                            {
+ 
+                                i--;
+ 
+                                printf("\b");
+ 
+                                printf(" ");
+ 
+                                printf("\b");
+ 
+                                pass[i]=getch();
+ 
+                            }
+ 
+                            else
+ 
+                            {
+ 
+                                printf("*");
+ 
+                                i++;
+ 
+                                pass[i]=getch();
+ 
+                            }
+ 
+                        }
+ 
+                        pass[i]='\0';
+ 
+                        i=0;
+ 
+                        printf("\n\tCONFIRM PASSWORD:");
+ 
+                        confirm[0]=getch();
+ 
+                        while(confirm[i]!='\r')
+ 
+                        {
+ 
+                            if(confirm[i]=='\b')
+ 
+                            {
+ 
+                                i--;
+ 
+                                printf("\b");
+ 
+                                printf(" ");
+ 
+                                printf("\b");
+ 
+                                confirm[i]=getch();
+ 
+                            }
+ 
+                            else
+ 
+                            {
+ 
+                                printf("*");
+ 
+                                i++;
+ 
+                                confirm[i]=getch();
+ 
+                            }
+ 
+                        }
+ 
+                        confirm[i]='\0';
+ 
+                        if(strcmp(pass,confirm)==0)
+ 
+                        {
+ 
+                            fp=fopen("SE","wb");
+ 
+                            if(fp==NULL)
+ 
+                            {
+ 
+                                printf("\n\t\tSYSTEM ERROR");
+ 
+                                getch();
+ 
+                                return ;
+ 
+                            }
+ 
+                            i=0;
+ 
+                            while(pass[i]!='\0')
+ 
+                            {
+ 
+                                ch=pass[i];
+ 
+                                putc(ch+5,fp);
+ 
+                                i++;
+ 
+                            }
+ 
+                            putc(EOF,fp);
+ 
+                            fclose(fp);
+ 
+                        }
+ 
+                        else
+ 
+                        {
+ 
+                            printf("\n\tTHE NEW PASSWORD DOES NOT MATCH.");
+ 
+                            choice=1;
+ 
+                        }
+ 
+                    }
+ 
+}while(choice==1);
+ 
+    printf("\n\n\tPASSWORD CHANGED...\n\n\tPRESS ANY KEY TO GO BACK...");
+ 
+    getch();
